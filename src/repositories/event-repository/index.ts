eb2dd75664b6a728117921543330eb8f8dd48290
= <=>
2022-10-28 14:05:27 -0300

    implementation_redis_feature

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,19 @@
-import { prisma } from '@/config';
+import { prisma, redisClient } from '@/config';
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cacheExpiration =10000
+
+  let eventRedis = await redisClient.get('eventKey')
+  if(eventRedis){
+    console.log(`eventRedis: ${eventRedis}`)
+    return JSON.parse(eventRedis)
+  }
+  const event = await prisma.event.findFirst();
+  console.log(event)
+  console.log(`eventPrisma: ${event}`)
+  redisClient.setEx("eventKey", cacheExpiration, JSON.stringify(event))
+
+  return event
 }
 
 const eventRepository = {
