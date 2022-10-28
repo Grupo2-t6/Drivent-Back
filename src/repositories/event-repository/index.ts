b10bab008f7f0baff463950d32a9f3511614cffc
= <=>
2022-10-28 14:08:52 -0300

    remove console.log

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -3,15 +3,13 @@ import { prisma, redisClient } from '@/config';
 async function findFirst() {
   const cacheExpiration =10000
 
-  let eventRedis = await redisClient.get('eventKey')
+  let eventRedis = await redisClient.get('eventKey1')
   if(eventRedis){
-    console.log(`eventRedis: ${eventRedis}`)
     return JSON.parse(eventRedis)
   }
   const event = await prisma.event.findFirst();
-  console.log(event)
   console.log(`eventPrisma: ${event}`)
-  redisClient.setEx("eventKey", cacheExpiration, JSON.stringify(event))
+  redisClient.setEx("eventKey1", cacheExpiration, JSON.stringify(event))
 
   return event
 }
