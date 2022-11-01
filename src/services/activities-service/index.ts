d0c1a057734f4bfed5e8b0714a5ae9e5eeb01aff
MariaDuda <mariabevenuto062@gmail.com>
2022-11-01 15:45:33 -0300

    feat: validate by activities choosed by user

--- src/services/activities-service/index.ts
+++ src/services/activities-service/index.ts
@@ -9,18 +9,31 @@ async function validateVacancieActivityByTime(userId: number, activityId: number
 }
 async function isTimeValid(activity: Activities) {
   const time = Number(activity.startTime[0] + activity.startTime[1]);
-  const firstSelected = {
-    id: 1,
-    date: 'sexta,22/10',
-    vacancies: 27,
-    startTime: '09:00',
-    name: 'MINECRAFT',
-    trailId: 1,
-    finalTime: '10:00',
-  };
-  const selectedActivityTime = Number(firstSelected.startTime[0] + firstSelected.startTime[1]);
-  if (time === selectedActivityTime && activity.date === firstSelected.date) {
-    throw conflictError('horários entram em conflito');
+  const firstSelected = [
+    {
+      id: 1,
+      date: 'sexta,22/10',
+      vacancies: 27,
+      startTime: '09:00',
+      name: 'MINECRAFT',
+      trailId: 1,
+      finalTime: '10:00',
+    },
+    {
+      id: 4,
+      date: 'sexta,23/10',
+      vacancies: 27,
+      startTime: '10:00',
+      name: 'MINECRAFT3',
+      trailId: 1,
+      finalTime: '11:00',
+    },
+  ];
+  for (const activityChoosed of firstSelected) {
+    const selectedActivityTime = Number(activityChoosed.startTime[0] + activityChoosed.startTime[1]);
+    if (time === selectedActivityTime && activity.date === activityChoosed.date) {
+      throw conflictError('horários entram em conflito');
+    }
   }
 }
 
