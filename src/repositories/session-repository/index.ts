2e9d32c5e9039eac5f2ff53b0b9372103be3a921
= <=>
2022-11-04 15:31:41 -0300

    fix OAuth

--- src/repositories/session-repository/index.ts
+++ src/repositories/session-repository/index.ts
@@ -2,6 +2,8 @@ import { prisma } from '@/config';
 import { Prisma } from '@prisma/client';
 
 async function create(data: Prisma.SessionUncheckedCreateInput) {
+  console.log(`dataInRepository:`)
+  console.log(data)
   return prisma.session.create({
     data,
   });
--- src/services/authentication-service/index.ts
+++ src/services/authentication-service/index.ts
@@ -6,6 +6,9 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { invalidCredentialsError } from './errors';
 
+import axios from 'axios';
+import qs from 'query-string';
+
 async function signIn(params: SignInParams): Promise<SignInResult> {
   const { email, password } = params;
 
@@ -28,7 +31,7 @@ async function getUserOrFail(email: string): Promise<GetUserOrFailResult> {
   return user;
 }
 
-async function createSession(userId: number) {
+async function createSession(userId: any) {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET);
   await sessionRepository.create({
     token,
@@ -52,8 +55,55 @@ type SignInResult = {
 
 type GetUserOrFailResult = Pick<User, 'id' | 'email' | 'password'>;
 
+
+export async function singInOauthPost(code: string){
+  
+  const token = await exchangeCodeForAccessToken(code);
+  const dataUser = await fetchUser(token)
+  
+  const isUser = await userRepository.findyUserById(dataUser.id)
+  if(!isUser){
+    await userRepository.create({id: dataUser.id, email:dataUser.email, password: 'passFake' })
+  }
+  console.log(dataUser)
+  console.log(dataUser.id)
+  return await createSession(dataUser.id)
+
+  async function exchangeCodeForAccessToken(code: string) {
+    console.log(code)
+    const GITHUB_ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token';
+    const { REDIRECT_URL, CLIENT_ID, CLIENT_SECRET } = process.env;
+    const params = {
+      code,
+      grant_type: 'authorization_code',
+      redirect_uri: REDIRECT_URL,
+      client_id: '8ffd828e28e1aa67ba4f',
+      client_secret: '2c1ec8c59ed461003aa3a0578b02a7c9b88791da'
+    };
+  
+    const { data } = await axios.post(GITHUB_ACCESS_TOKEN_URL, params, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    const parsedData = qs.parse(data);
+    return parsedData.access_token;
+  }
+
+  async function fetchUser(token: any) {
+    const response = await axios.get('https://api.github.com/user', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return response.data
+  }
+}
+
 const authenticationService = {
   signIn,
+  singInOauthPost
 };
 
 export default authenticationService;
