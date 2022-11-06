6464a3383268d0bf9bf8d5d3b7cd1793b3b4faad
Lucas <lucasbatista2408@gmail.com>
2022-11-06 12:30:52 -0300

    teste2

--- src/routers/activities-router.ts
+++ src/routers/activities-router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { postActivity, activityVacancies} from '@/controllers/activities-controller';
+import { postActivity, activityVacancies, getActivities} from '@/controllers/activities-controller';
 
 const activitiesRouter = Router();
 
 activitiesRouter.post('/:id', authenticateToken, postActivity);
 activitiesRouter.get('/:id', activityVacancies);
+activitiesRouter.get('/getA', getActivities)
 
 export { activitiesRouter };
